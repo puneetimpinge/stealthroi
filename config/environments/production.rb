5051306090c61f7ab0ef229e22fbf1ab82735369
Ritika90 <erritika.1690@gmail.com>
2015-06-22 11:17:25 +0530

    file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.action_mailer.default_url_options = { host: 'http://stealth-app.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
