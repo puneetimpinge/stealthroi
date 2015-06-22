aa50d1106770502a8ea349139aa202a683ac2fec
Ritika90 <erritika.1690@gmail.com>
2015-06-22 10:43:28 +0530

    changed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
+  config.action_mailer.default_url_options = { host: 'http://stealth-app.herokuapp.com' }
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
