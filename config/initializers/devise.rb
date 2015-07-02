f62fd4284e80df3b01a1488d4fb15d8ac60bceec
Ritika90 <erritika.1690@gmail.com>
2015-07-02 12:45:17 +0530

    added application.yml file for keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,5 +257,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   config.secret_key = '4f1f9020a32ab00ffb68959772b1146413dc2820a295e41fc5b0c88e3df95bf29c98dad48d0b84aa0646d97df3eb86e45ea56fd0990c09fe4781940b06af6afc'
-  config.omniauth :facebook, "784389335013233", "6ba98f5a049a440d964cf54e7a2f46e2"
+  config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_secret']
 end
