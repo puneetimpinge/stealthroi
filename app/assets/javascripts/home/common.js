fc5e2e59b2f6e48b26f57a95e603d48c8d902e59
Ritika90 <erritika.1690@gmail.com>
2015-07-15 10:34:22 +0530

    popup size

--- app/assets/javascripts/home/common.js
+++ app/assets/javascripts/home/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('a.popup').click(function(e) {
     id = $(this).attr("id")
-    var width = 600, height = 400;
+    var width = 600, height = 450;
     var left = (screen.width / 2) - (width / 2);
     var top = (screen.height / 2) - (2 * height / 3);
     var features = 'menubar=no,toolbar=no,status=no,width=' + width + ',height=' + height + ',toolbar=no,left=' + left + ',top=' + top;
