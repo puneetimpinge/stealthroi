2e41b125304d137596b7c598f0c4e9faafbac6b6
Ritika90 <erritika.1690@gmail.com>
2015-07-20 13:56:55 +0530

    get_top_campaigns api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -25,9 +25,17 @@ class HomeController < ApplicationController
   end	
 
   def get_top_campaigns
-  	response = HTTParty.post('http://stealthroi.com/phpscripts/topCampaignHandlerScript.php',
-      body: params[:parameters] )
-  	render :json => { :response => response }.to_json
+    startHour = Time.at(Time.now.utc + Time.zone_offset('EDT')).strftime('%Y-%m-%d %H:00:00').gsub(" ","%20")
+    lastHour = Time.at(Time.now.utc + Time.zone_offset('EDT') - 1.hour).strftime('%Y-%m-%d %H:00:00').gsub(" ","%20")
+    nowPlusFifteenTimeStamp = Time.at(Time.now.utc + Time.zone_offset('EDT') + 15.days).strftime('%Y-%m-%d %H:00:00').gsub(" ","%20")
+
+    response = HTTParty.get("https://viralstyle.com/api/v1/campaigns?end_date_start=#{lastHour}&end_date_end=#{nowPlusFifteenTimeStamp}",
+     headers: {"X-Authorization" => "e0cc76b0174030fb70d9a0593b09c34edeb2d5f7"})
+
+    render :json => { :response => "#{response['data'].count}customSplitterUnavailable" }.to_json
+  	# response = HTTParty.post('http://stealthroi.com/phpscripts/topCampaignHandlerScript.php',
+   #    body: params[:parameters] )
+  	# render :json => { :response => response }.to_json
   end
 
   def get_top_graph
